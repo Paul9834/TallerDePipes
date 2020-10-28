cb0bf913de6547c98d053029ead5d17ca31f6b24
Kevin Paul Montealegre Melo <kmontealegremelo@gmail.com>
2020-10-27 23:53:10 -0500

    Se añadieron los pipes al proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MyPipePipe } from './pipes/Letras/my-pipe.pipe';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TallerDePipes';
+  date1 = '2020/10/27, 23:00';
+  date2 = '2020/10/27, 20:04';
+  date3 = '2020/10/26, 21:38';
+  date4 = '2020/10/25, 21:38';
+  date5 = '2020/10/25, 21:38';
+  date6 = '2020/05/30, 21:38';
+  date7 = '2020/05/27, 21:38';
+  num1 = 5;
+  var1= 'amigo';
+  var2= 'amiga estas muy linda';
+  var3= 'miga estas muy linda';
+  constructor() {
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './pipes/Letras/app-routing.module';
 import { AppComponent } from './app.component';
+import { MyPipePipe } from './pipes/Letras/my-pipe.pipe';
+import { PipeElevadoPipe } from './pipes/Potencia/pipe-elevado.pipe';
+import { Date1Pipe } from './pipes/Ejercicio-1/date1.pipe';
+import { Date2Pipe } from './pipes/Ejercicio-2/date2.pipe';
+import { Date3Pipe } from './pipes/Ejercicio-3/date3.pipe';
+import { Date4Pipe } from './pipes/Ejercicio-4/date4.pipe';
+import { Date5Pipe } from './pipes/Ejercicio-5/date5.pipe';
+import { Date6Pipe } from './pipes/Ejercicio-6/date6.pipe';
+import { Date7Pipe } from './pipes/Ejercicio-7/date7.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyPipePipe,
+    PipeElevadoPipe,
+    Date1Pipe,
+    Date2Pipe,
+    Date3Pipe,
+    Date4Pipe,
+    Date5Pipe,
+    Date6Pipe,
+    Date7Pipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
